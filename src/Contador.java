f63fcd4c2a26dde424fdca72593996bdf715d9ab
DamasX <84998412+TarcisioDamascena@users.noreply.github.com>
2024-05-15 09:22:21 -0300

    Update: Classe Contador

--- src/Contador.java
+++ src/Contador.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Contador {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Bem vindo ao contador!");
+
+        System.out.println("Olá, seja bem vindo ao contador!");
 
         System.out.println("Digite o primeiro número: ");
         int numeroUm = scanner.nextInt();
@@ -15,21 +16,22 @@ public class Contador {
 
         try {
             contar(numeroUm, numeroDois);
-        } catch (ParametroInvalidoException e) {
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+
     }
 
     static void contar(int numUm, int numDois) throws ParametroInvalidoException {
         if (numUm > numDois) {
-            throw new ParametroInvalidoException("O primeiro número é maior que o segundo");
+            throw new ParametroInvalidoException("O primeiro número é maior que o segundo.");
         } else {
             int contagem = numDois - numUm;
-            System.out.println("De " + numUm + " a " + numDois + " são: ");
-            for (int i = 1; i <= contagem; i++) {
-
-                System.out.println("contando " + i);
+            System.out.println("Contando...");
+            for (int i = 1; i <= contagem; i++){
+                System.out.println("Número " + i);
             }
+            System.out.println("Terminou a contagem!");
         }
 
     }
